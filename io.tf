14a3c9b522b5c3e0b0000025fe1978147208aaa5
Halfdan Rump <dalekk@gmail.com>
2019-08-15 11:15:43 +0900

    Adds type and description to variables

--- io.tf
+++ io.tf
@@ -1,39 +1,67 @@
-variable account_id{} # something that looks like 211377887982
+variable account_id{
+  type        = "string"
+  description = "Your 12 digit numeric AWS account id"
+  }
 
-variable name {}  # project name
+variable name {
+  type        = "string"
+  description = "Name of the project. All resources will include the name. "
+}
 
-variable environment {}  # name of the environment, e.g. staging or production
+variable environment {
+  type        = "string"
+  description = "Name of the runtime environment, e.g. `staging` or `production`."
+  }
 
-variable github_webhook_token {}  #
+variable github_webhook_token {
+  type        = "string"
+  description = "A secret github token used to setup the github webhook that will trigger the CI pipeline."
+  }  #
 
-variable git_repo {} # name of the github repo
+variable git_repo {
+  type        = "string"
+  description = "Name of the github repo."
+}
 
 variable git_branch {
-  default = "production"
+  type        = "string"
+  description = "The name of the branch that, when pushed to, will trigger the CI pipeline."
+  default     = "master"
   }
 
 variable git_organization {
-  default = "SnapDish"
+  type        = "string"
+  description = "Name of the github organization that the repository belongs to."
+  default     = "SnapDish"
   }
 
 variable build_image {
-  default = "aws/codebuild/docker:18.09.0"
+  type        = "string"
+  description = "Name of the image that will be used to in the piepline build stage."
+  default     = "aws/codebuild/docker:18.09.0"
 }
 
 variable dockerbuild_timeout {
-  default = 5
+  type        = "string"
+  description = "Timeout in minutes of the docker build."
+  default     = 5
 }
 
 variable unittest_image {
-  default = "aws/codebuild/python:3.6.5"
+  type        = "string"
+  description = "Name of the image that will be used to run unit tests."
+  default     = "aws/codebuild/python:3.6.5"
 }
 
 variable unittest_timeout {
-  default = 15
+  type        = "string"
+  description = "Timeout in minutes of the unit test pipeline stage."
+  default     = 15
 }
 
 variable unittest_vpc {
-
+  type        = "string"
+  description = "id of the vpc in which unit tests are run."
 }
 
 variable unittest_subnets {
@@ -47,6 +75,12 @@ variable unittest_security_groups {
 }
 
 
-variable unittest_buildspec_path {}
+variable unittest_buildspec_path {
+  type        = "string"
+  description = "path to the buildspec file used to run the unit tests."
+}
 
-variable dockerbuild_buildspec_path {}
+variable dockerbuild_buildspec_path {
+  type        = "string"
+  description = "path to the buildspec file used to build the docker image."
+}
